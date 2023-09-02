56ad52922b936fe682128378a468657fbca62699
SUDHANSHU SINGH <Sudhanshusingh705495@gmail.com>
2023-09-02 16:00:35 +0530

    "some changes"

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    presets: [
+      // Add your desired presets here, e.g., "@babel/preset-react"
+    ],
+    plugins: [
+      // Add other plugins if needed
+      "@babel/plugin-proposal-private-property-in-object"
+    ]
+  };
+  
